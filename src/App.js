179031fbd18791860c7d2de3a626ca13e1d4edf8
Yuta Toyokawa <kppg42@gmail.com>
2018-11-29 20:22:02 +0900

    display contentful content

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+      body: ''
+    };
+  }
+
+  componentDidMount() {
+    this.setBlogContent();
+  }
+
+  setBlogContent() {
+    const spaceId = 'rlz1oc4sgyfk';
+    const accessToken = 'f5dfdf9b1672f3cbbf5030c269fff9d9a0b349ed42a2a66b3d56ed747185603a';
+    const url = `https://cdn.contentful.com/spaces/${spaceId}/entries`;
+
+    axios.get(url, { headers: { 'Authorization': `Bearer ${accessToken}`}})
+      .then(res => {
+        const blogItem = res.data.items[0].fields;
+        this.setState({
+          title: blogItem.title,
+          body: blogItem.body
+        })
+      });
+  }
+  
   render() {
     return (
       <div>
-        <h2>Hello World</h2>
+        <h2>{this.state.title}</h2>
+        <p>{this.state.body}</p>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,15 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import axios from 'axios';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
 serviceWorker.register();
-
-const spaceId = 'rlz1oc4sgyfk';
-const accessToken = 'f5dfdf9b1672f3cbbf5030c269fff9d9a0b349ed42a2a66b3d56ed747185603a';
-const url = `https://cdn.contentful.com/spaces/${spaceId}/entries`;
-
-axios.get(url, { headers: { 'Authorization': `Bearer ${accessToken}`}})
-        .then(res => console.log(res.data.items[0]));
