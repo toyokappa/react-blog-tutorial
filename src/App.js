b346931be34c683b7bf1ba062c070dc32a8bf439
Yuta Toyokawa <kppg42@gmail.com>
2018-11-29 22:04:56 +0900

    formatter by prettier

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React, { Component } from 'react';
-import axios from 'axios';
-import ReactMarkdown from 'react-markdown';
-import * as moment from 'moment';
+import React, { Component } from "react";
+import axios from "axios";
+import ReactMarkdown from "react-markdown";
+import * as moment from "moment";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      title: '',
-      body: '',
-      createdAt: '',
-      updatedAt: ''
+      title: "",
+      body: "",
+      createdAt: "",
+      updatedAt: ""
     };
   }
 
@@ -19,30 +19,32 @@ class App extends Component {
   }
 
   setBlogContent() {
-    const spaceId = 'rlz1oc4sgyfk';
-    const accessToken = 'f5dfdf9b1672f3cbbf5030c269fff9d9a0b349ed42a2a66b3d56ed747185603a';
+    const spaceId = "rlz1oc4sgyfk";
+    const accessToken =
+      "f5dfdf9b1672f3cbbf5030c269fff9d9a0b349ed42a2a66b3d56ed747185603a";
     const url = `https://cdn.contentful.com/spaces/${spaceId}/entries`;
 
-    axios.get(url, { headers: { 'Authorization': `Bearer ${accessToken}`}})
+    axios
+      .get(url, { headers: { Authorization: `Bearer ${accessToken}` } })
       .then(res => {
         const blogItem = res.data.items[0];
-        console.log(blogItem)
+        console.log(blogItem);
         this.setState({
           title: blogItem.fields.title,
           body: blogItem.fields.body,
           createdAt: blogItem.sys.createdAt,
           updatedAt: blogItem.sys.updatedAt
-        })
+        });
       });
   }
-  
+
   render() {
-    const { title, body, createdAt, updatedAt } = this.state
+    const { title, body, createdAt, updatedAt } = this.state;
     return (
       <div>
         <h1>{title}</h1>
-        <div>作成日: {moment(createdAt).format('YYYY.MM.DD HH:mm')}</div>
-        <div>更新日: {moment(updatedAt).format('YYYY.MM.DD HH:mm')}</div>
+        <div>作成日: {moment(createdAt).format("YYYY.MM.DD HH:mm")}</div>
+        <div>更新日: {moment(updatedAt).format("YYYY.MM.DD HH:mm")}</div>
         <ReactMarkdown source={body} />
       </div>
     );
