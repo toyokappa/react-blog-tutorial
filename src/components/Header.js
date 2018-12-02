63a0f1fe909f53ca6852c0dfc40c1cc4083ca4ed
Yuta Toyokawa <kppg42@gmail.com>
2018-12-02 16:18:29 +0900

    change design

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 const Footer = () => {
   return (
     <div>
-      <div className="logo">Blog Tutorial</div>
+      <h1 className="logo">Blog Tutorial</h1>
       <ul>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/about">About</Link></li>
