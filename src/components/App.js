8485941f290031d9be5b035221221658b3d2954e
Yuta Toyokawa <kppg42@gmail.com>
2018-12-02 16:16:41 +0900

    delete footer

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,12 @@ import React from "react";
 
 import Header from "./Header";
 import Wrapper from "./Wrapper";
-import Footer from "./Footer";
 
 const App = () => {
   return (
     <div>
       <Header />
       <Wrapper />
-      <Footer />
     </div>
   );
 };
