0ef97250d8baf7deddd5b290bb6a19f3aab7b511
Yuta Toyokawa <kppg42@gmail.com>
2018-12-03 15:13:26 +0900

    display eyecatch from contentful

--- src/components/BlogItem.js
+++ src/components/BlogItem.js
@@ -10,6 +10,7 @@ class BlogIndex extends Component {
       isError: false,
       errorMsg: "",
       title: "",
+      eyeCatchUrl: "https://via.placeholder.com/600x400?text=NoImage",
       body: "",
       createdAt: "",
       updatedAt: ""
@@ -31,6 +32,8 @@ class BlogIndex extends Component {
       .get(url, { headers: { Authorization: `Bearer ${accessToken}` } })
       .then(res => {
         const blogItem = res.data;
+        const eyeCatchId = blogItem.fields.eyeCatch.sys.id
+        if (eyeCatchId) this.setBlogEyeCatch(eyeCatchId);
         this.setState({
           title: blogItem.fields.title,
           body: blogItem.fields.body,
@@ -51,13 +54,28 @@ class BlogIndex extends Component {
       });
   }
 
+  setBlogEyeCatch(id) {
+    const spaceId = "rlz1oc4sgyfk";
+    const url = `https://cdn.contentful.com/spaces/${spaceId}/assets/${id}`;
+    const accessToken =
+      "f5dfdf9b1672f3cbbf5030c269fff9d9a0b349ed42a2a66b3d56ed747185603a";
+
+    axios
+      .get(url, { headers: { Authorization: `Bearer ${accessToken}` } })
+      .then(res => {
+        const eyeCatchUrl = res.data.fields.file.url;
+        this.setState({ eyeCatchUrl: eyeCatchUrl });
+      });
+  }
+
   render() {
-    const { isError, errorMsg, title, body, createdAt, updatedAt } = this.state;
+    const { isError, errorMsg, title, eyeCatchUrl, body, createdAt, updatedAt } = this.state;
     if (isError) return <div>{errorMsg}</div>;
 
     return (
       <div>
         <h1>{title}</h1>
+        <img src={eyeCatchUrl} alt="eye_catch" />
         <div>作成日: {moment(createdAt).format("YYYY.MM.DD HH:mm")}</div>
         <div>更新日: {moment(updatedAt).format("YYYY.MM.DD HH:mm")}</div>
         <ReactMarkdown source={body} />
