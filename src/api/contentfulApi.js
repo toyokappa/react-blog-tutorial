72bb6a976ed26fd6c923c08a7541fdb6233c63f2
Yuta Toyokawa <kppg42@gmail.com>
2018-12-05 10:28:46 +0900

    use prettier from eslint

--- src/api/contentfulApi.js
+++ src/api/contentfulApi.js
@@ -5,17 +5,23 @@ const accessToken = "f5dfdf9b1672f3cbbf5030c269fff9d9a0b349ed42a2a66b3d56ed74718
 
 const getEntries = () => {
   const url = `https://cdn.contentful.com/spaces/${spaceId}/entries`;
-  return axios.get(url, { headers: { Authorization: `Bearer ${accessToken}` } });
+  return axios.get(url, {
+    headers: { Authorization: `Bearer ${accessToken}` }
+  });
 };
 
 const getEntry = entryId => {
   const url = `https://cdn.contentful.com/spaces/${spaceId}/entries/${entryId}`;
-  return axios.get(url, { headers: { Authorization: `Bearer ${accessToken}` } });
+  return axios.get(url, {
+    headers: { Authorization: `Bearer ${accessToken}` }
+  });
 };
 
 const getAsset = assetId => {
   const url = `https://cdn.contentful.com/spaces/${spaceId}/assets/${assetId}`;
-  return axios.get(url, { headers: { Authorization: `Bearer ${accessToken}` } });
+  return axios.get(url, {
+    headers: { Authorization: `Bearer ${accessToken}` }
+  });
 };
 
 export { getEntries, getEntry, getAsset };
